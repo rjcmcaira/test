f880d67d92c8551b77b277196b7ec7d157070563
JOSE CAIRA <26josecaira123@gmail.com>
2022-11-16 09:49:39 -0500

    16/11/2022 Produccion Heroku

--- config/conexion.php
+++ config/conexion.php
@@ -3,11 +3,16 @@
         protected $dbh;
         protected function Conexion(){
             try{
-                $conectar = $this -> dbh = new PDO("mysql:local=localhost;dbname=paginawebphp","root","");
-                return $conectar;
-                //pruebas de conexion a un servidor en ala nube
+                //conexion local
+                //$conectar = $this -> dbh = new PDO("mysql:local=localhost;dbname=paginawebphp","root","");
+                //return $conectar;
+
+                //pruebas de conexion a un servidor en ala nube GODADDY
                 //$conectar = $this -> dbh = new PDO("mysql:local=localhost;dbname=paginawebphp","webtemplate","@andercode");
-                //puesta de produccion en la nube
+
+                //heroku
+                $conectar = $this -> dbh = new PDO("mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_4898f3d5f89721f","b915116952ed03","635ee4a7");
+                return $conectar;
 
             }catch(Exception $e){
                 print "Error :".$e->getMessage();
